1accc760fc324ad464d37f439ff15b0d15ab17d6
Lingzhiwei <zhiweiling1999@outlook.com>
2026-01-14 11:44:41 +0800

    add maxlogit

--- baselines/dnn_based/__init__.py
+++ baselines/dnn_based/__init__.py
@@ -1,7 +1,9 @@
 from .msp import MSP
 from .energy import Energy
+from .maxlogit import MaxLogit
 
 __all__ = [
     "msp",
     "energy",
+    "maxlogit",
 ]
---
+++ baselines/dnn_based/maxlogit.py
@@ -0,0 +1,30 @@
+from baselines.base import BaseBaseline
+from baselines.registry import register_baseline
+
+import torch.nn.functional as F
+import torch
+import numpy as np
+from tqdm import tqdm
+
+
+@register_baseline("maxlogit")
+class MaxLogit(BaseBaseline):
+    def __init__(self, model, args):
+        self.model = model
+        self.device = args.device
+
+    @torch.no_grad()
+    def eval(self, data_loader):
+        self.model.eval()
+        result = []
+        
+        for (images, _) in tqdm(data_loader):
+            images = images.to(self.device)
+            output = self.model.get_output(images)
+
+            output = output.data.cpu().numpy()
+            output = np.max(output, axis=1)
+
+            result.append(output)
+
+        return np.concatenate(result)
